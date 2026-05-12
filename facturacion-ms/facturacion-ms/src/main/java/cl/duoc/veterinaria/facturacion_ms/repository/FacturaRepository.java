86dc0d4851132b6770e0985295f65137e32e0a44
Ignacio Ochoa <167282214+Nacho9898@users.noreply.github.com>
2026-05-12 04:06:29 -0400

    Delete facturacion-ms/facturacion-ms directory

--- facturacion-ms/facturacion-ms/src/main/java/cl/duoc/veterinaria/facturacion_ms/repository/FacturaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.veterinaria.facturacion_ms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import cl.duoc.veterinaria.facturacion_ms.model.Factura;
-
-@Repository
-public interface FacturaRepository extends JpaRepository<Factura, Long> {
-}
